7fd1e4bdf38f2493cd95800c979ff04a1927edd8
Kristian <kpind@aarhus.dk>
2019-10-09 13:40:22 +0200

    update dbf_check

--- dbf_check.sql
+++ dbf_check.sql
@@ -1,8 +1,13 @@
-select dagsorden_indstilling.afsender, count(*) from fil
-inner join indstilling_fil on fil.fil_id = indstilling_fil.fil_id
-inner join indstilling on indstilling_fil.indstilling_id = indstilling.indstilling_id
-inner join dagsorden_indstilling on indstilling.indstilling_id = dagsorden_indstilling.indstilling_id
-inner join dagsorden on dagsorden_indstilling.dagsorden_id = dagsorden.dagsorden_id
-where fil.filename = 'tb_t_ba.dbf'
-group by dagsorden_indstilling.afsender 
-order by count(*) desc
+SELECT dagsorden_indstilling.afsender, count(*) 
+FROM fil
+INNER JOIN indstilling_fil 
+ON fil.fil_id = indstilling_fil.fil_id
+INNER JOIN indstilling 
+ON indstilling_fil.indstilling_id = indstilling.indstilling_id
+INNER JOIN dagsorden_indstilling 
+ON indstilling.indstilling_id = dagsorden_indstilling.indstilling_id
+INNER JOIN dagsorden 
+ON dagsorden_indstilling.dagsorden_id = dagsorden.dagsorden_id
+WHERE fil.filename = 'tb_t_ba.dbf'
+GROUP BY dagsorden_indstilling.afsender 
+ORDER BY count(*) desc
