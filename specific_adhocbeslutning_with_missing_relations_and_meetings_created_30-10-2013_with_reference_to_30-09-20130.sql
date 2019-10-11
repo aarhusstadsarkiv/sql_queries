edb4820bd60246f1afb5728fd86e3eda08126f0c
Kristian <kpind@aarhus.dk>
2019-10-11 14:47:06 +0200

    Create sql query

---
+++ specific_adhocbeslutning_with_missing_relations_and_meetings_created_30-10-2013_with_reference_to_30-09-20130.sql
@@ -0,0 +1,7 @@
+SELECT *
+FROM adhocbeslutning
+LEFT JOIN adhoc_adhocbeslutning
+ON adhocbeslutning.adhocbeslutning_id = adhoc_adhocbeslutning.adhocbeslutning_id
+LEFT JOIN adhoc
+ON adhoc_adhocbeslutning.adhoc_id = adhoc.adhoc_id
+WHERE adhoc.adhoc_id IS NULL AND adhocbeslutning.BeslutResume IS NOT 'Beslutning ikke frigivet' AND adhocbeslutning.CreatedDate = '2013-10-30' AND adhocbeslutning.BeslutResume LIKE 'Udvalget ønskede oplyst proceduren for opsætning af vejbelysning på Bredgade i forbindelse%'
