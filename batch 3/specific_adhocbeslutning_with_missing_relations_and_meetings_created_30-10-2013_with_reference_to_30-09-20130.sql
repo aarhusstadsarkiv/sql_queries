d5f5558f6a3fc157408d752a65934843a4b05c1a
Kristian Pindstrup <56304784+kpind-aarhus@users.noreply.github.com>
2019-11-14 14:08:13 +0100

    Update specific_adhocbeslutning_with_missing_relations_and_meetings_created_30-10-2013_with_reference_to_30-09-20130.sql

--- batch 3/specific_adhocbeslutning_with_missing_relations_and_meetings_created_30-10-2013_with_reference_to_30-09-20130.sql
+++ batch 3/specific_adhocbeslutning_with_missing_relations_and_meetings_created_30-10-2013_with_reference_to_30-09-20130.sql
@@ -4,4 +4,4 @@ LEFT JOIN adhoc_adhocbeslutning
 ON adhocbeslutning.adhocbeslutning_id = adhoc_adhocbeslutning.adhocbeslutning_id
 LEFT JOIN adhoc
 ON adhoc_adhocbeslutning.adhoc_id = adhoc.adhoc_id
-WHERE adhoc.adhoc_id IS NULL AND adhocbeslutning.BeslutResume IS NOT 'Beslutning ikke frigivet' AND adhocbeslutning.CreatedDate = '2013-10-30' AND adhocbeslutning.BeslutResume LIKE 'Udvalget ønskede oplyst proceduren for opsætning af vejbelysning på Bredgade i forbindelse%'
+WHERE adhoc.adhoc_id IS NULL AND adhocbeslutning.BeslutResumeString IS NOT 'Beslutning ikke frigivet' AND adhocbeslutning.CreatedDate = '2013-10-30' AND adhocbeslutning.BeslutResumeString LIKE 'Udvalget ønskede oplyst proceduren for opsætning af vejbelysning på Bredgade i forbindelse%'
